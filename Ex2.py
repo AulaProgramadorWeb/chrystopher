e349fab3cf529346ed233a1844ee0dd18d3af19f
chrystopherj <134288043+chrystopherj@users.noreply.github.com>
2023-05-22 21:34:32 -0300

    Delete Ex2.py

--- Ex2.py
+++
@@ -1,8 +0,0 @@
-pao = float(input("Quantos pães?"))
-sonho = float(input("Quantos sonhos?"))
-
-pao= pao * 0.5
-sonho = sonho * 2.0
-resultado = pao + sonho
-
-print("valor a pagar é: R$", resultado)
