7bf69d25156510ecc4ac1fb6a5ea59e7749192f6
chrystopherj <134288043+chrystopherj@users.noreply.github.com>
2023-05-23 22:50:39 -0300

    Delete Ex5.py

--- Ex5.py
+++
@@ -1,7 +0,0 @@
-vcompra = float(input("Insira o valor da sua compra: "))
-desc = float(input("Insira o valor do desconto: "))
-
-desc = desc / 100
-vcompra = vcompra -(vcompra*desc)
-
-print("o valor da sua compra com desconto é: ", vcompra)
